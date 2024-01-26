71d11cb538d0b0c8749f7d92ef32aa8ddc3dc30e
Paige Rubendall <prubenda@redhat.com>
2024-01-26 16:04:48 -0500

    fixing spaces
    Signed-off-by: Paige Rubendall <prubenda@redhat.com>

--- orion.py
+++ orion.py
@@ -118,8 +118,8 @@ def get_metadata(test,logger):
         dict: dictionary of the metadata
     """
     metadata = {}
-    for k,v in test.items(): 
-        if k in ["metrics","name"]: 
+    for k,v in test.items():
+        if k in ["metrics","name"]:
             continue
         metadata[k] = v
     metadata["ocpVersion"] = str(metadata["ocpVersion"])
