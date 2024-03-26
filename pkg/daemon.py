1327774128c228540ec30a9cb90929213c9f9fa6
Shashank Reddy Boyapally <sboyapal@redhat.com>
2024-03-25 23:49:27 -0400

    changed accordingly to fmatch 0.0.6, and filtering changepoints
    Signed-off-by: Shashank Reddy Boyapally <sboyapal@redhat.com>

--- pkg/daemon.py
+++ pkg/daemon.py
@@ -1,6 +1,7 @@
 """
 Module to run orion in daemon mode
 """
+
 import logging
 import shutil
 import os
@@ -15,7 +16,12 @@ logger_instance = SingletonLogger(debug=logging.INFO).logger
 
 
 @app.post("/daemon")
-async def daemon(file: UploadFile = File(...), uuid: str = "", baseline: str = ""):
+async def daemon(
+    file: UploadFile = File(...),
+    uuid: str = "",
+    baseline: str = "",
+    filter_changepoints="",
+):
     """starts listening on port 8000 on url /daemon
 
     Args:
@@ -28,15 +34,21 @@ async def daemon(file: UploadFile = File(...), uuid: str = "", baseline: str = "
     new_file_name = f"{file_name}_copy{file_extension}"
     with open(new_file_name, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    argDict={
-        'config': new_file_name,
-        'output_path': "output.csv",
-        'hunter_analyze': True,
-        'output_format': "json",
-        'uuid':uuid,
-        'baseline':baseline,
+    argDict = {
+        "config": new_file_name,
+        "output_path": "output.csv",
+        "hunter_analyze": True,
+        "output_format": "json",
+        "uuid": uuid,
+        "baseline": baseline,
     }
+    filter_changepoints = (
+        True if filter_changepoints == "true" else False # pylint: disable = R1719
+    )
     result = runTest.run(**argDict)
+    if filter_changepoints:
+        for key, value in result.items():
+            result[key] = list(filter(lambda x: x.get("is_changepoint", False), value))
     try:
         os.remove(new_file_name)
     except OSError as e:
