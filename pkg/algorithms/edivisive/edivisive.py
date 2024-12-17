b1fb3c42654267c4cb5ca0c2aba5e7a10c8f4526
Joe Talerico aka rook <joe.talerico@gmail.com>
2024-12-17 12:28:40 -0500

    Addressing Nits Updating README
    Took Vishnu's feedback.
    Updated README
    Adding `reason` to example ack file
    
    Signed-off-by: Joe Talerico aka rook <joe.talerico@gmail.com>

--- pkg/algorithms/edivisive/edivisive.py
+++ pkg/algorithms/edivisive/edivisive.py
@@ -21,28 +21,17 @@ class EDivisive(Algorithm):
 
 
         # Process if we have ack'ed regression
-        ackList = []
+        ackSet = set()
         if len(self.options["ack"]) > 1 :
             for ack in self.options["ackMap"]["ack"]:
                 pos = series.find_by_attribute("uuid",ack["uuid"])
-                ackList.append(
-                    {"pos" : pos[0],
-                     "metric" : ack["metric"]})
+                ackSet.add(str(pos[0]) + "_" + ack["metric"])
 
-        # filter by direction
+        # filter by direction and ack'ed issues
         for metric, changepoint_list in change_points_by_metric.items():
             for i in range(len(changepoint_list)-1, -1, -1):
-                if ((self.metrics_config[metric]["direction"] == 1 and changepoint_list[i].stats.mean_1 > changepoint_list[i].stats.mean_2) or
-                    (self.metrics_config[metric]["direction"] == -1 and changepoint_list[i].stats.mean_1 < changepoint_list[i].stats.mean_2) ):
+                if ((self.metrics_config[metric]["direction"] == 1 and changepoint_list[i].stats.mean_1 > changepoint_list[i].stats.mean_2) or (self.metrics_config[metric]["direction"] == -1 and changepoint_list[i].stats.mean_1 < changepoint_list[i].stats.mean_2) or (str(changepoint_list[i].index) + "_" + changepoint_list[i].metric in ackSet)):
                     del changepoint_list[i]
-
-        # Filter ack'ed changes
-        for metric, changepoint_list in change_points_by_metric.items():
-            for i in range(len(changepoint_list)-1, -1, -1):
-                for acked in ackList:
-                    if len(changepoint_list) > 0 :
-                        if (changepoint_list[i].index == acked["pos"] and changepoint_list[i].metric == acked["metric"]):
-                            del changepoint_list[i]
 
         if [val for li in change_points_by_metric.values() for val in li]:
             self.regression_flag=True
