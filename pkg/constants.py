242a9b36267098e6481174a55b4d6f99c62db5bf
Raul Sevilla <rsevilla@redhat.com>
2025-07-16 10:13:54 +0200

    Fix timestamp standarization logic
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- pkg/constants.py
+++ pkg/constants.py
@@ -4,17 +4,9 @@ pkg.constants
 Module for storing constants across orion.
 """
 
-import re
-
 EDIVISIVE="EDivisive"
 ISOLATION_FOREST="IsolationForest"
 JSON="json"
 TEXT="text"
 JUNIT="junit"
 CMR="cmr"
-# Matches ISO 8601 datetime strings with nanosecond precision
-NANO_SECONDS_PATTERN = re.compile(r"^\d{4}-\d{2}-\d{2}T"
-                        r"\d{2}:\d{2}:\d{2}\."
-                        r"\d{9}Z$")
-# Matches Unix epoch timestamps in seconds
-EPOCH_TIMESTAMP_PATTERN = re.compile(r"^\d{10}$")
