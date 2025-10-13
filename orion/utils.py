79ede25a24d40110c948e4dbd0384a400629ffc1
Abraham <apalanis@redhat.com>
2025-10-13 14:08:36 -0400

    Bug fix for parsing nested tags in json, standardize timestamp to accept int
    Signed-off-by: Abraham <apalanis@redhat.com>

--- orion/utils.py
+++ orion/utils.py
@@ -142,7 +142,9 @@ class Utils:
         """
         if timestamp is None:
             return timestamp
-        if timestamp.isnumeric():
+        if isinstance(timestamp,(int,float)):
+            dt = pd.to_datetime(timestamp, unit='s', utc=True)
+        elif timestamp.isnumeric():
             dt = pd.to_datetime(timestamp, unit='s', utc=True)
         else:
             dt = pd.to_datetime(timestamp, utc=True)
@@ -246,6 +248,11 @@ class Utils:
             timestamp_field (str): timestamp field in data
         """
         test = match.get_results("", uuids, {}, timestamp_field=timestamp_field)
+        if "." in self.version_field:
+            return {
+                run[self.uuid_field]: match.dotDictFind(run, self.version_field)
+                for run in test
+            }
         return {run[self.uuid_field]: run[self.version_field] for run in test}
 
     def get_build_urls(self, uuids: List[str], match: Matcher, timestamp_field: str):
