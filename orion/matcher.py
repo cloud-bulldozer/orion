a44eb587ab6b56ad7b797992556b1d9179cd628d
Abraham <apalanis@redhat.com>
2025-10-13 20:07:48 -0400

    update docs for timestamp and uuid
    Signed-off-by: Abraham <apalanis@redhat.com>

--- orion/matcher.py
+++ orion/matcher.py
@@ -161,13 +161,13 @@ class Matcher:
         all_hits = self.query_index(s,return_all=True)
         uuids_docs = []
         for hit in all_hits:
-            if "." in self.version_field :
+            if "." in self.version_field:
                 value = self.dotDictFind(hit.to_dict()["_source"], self.version_field)
                 uuid_doc = {
                         self.uuid_field: hit.to_dict()["_source"][self.uuid_field],
                         self.version_field: value
                     }
-            else :
+            else:
                 uuid_doc = {
                         self.uuid_field: hit.to_dict()["_source"][self.uuid_field],
                         self.version_field: hit.to_dict()["_source"][self.version_field]
